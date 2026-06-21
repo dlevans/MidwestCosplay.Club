14d8b6bdfa44c65cc2ab9359e0a55d4f55569da9
dustin evans <evansdustin@hotmail.com>
2026-06-21 17:06:26 -0500

    google analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Updated import for ReactDOM
 import App from "./App";
+import ReactGA from 'react-ga4';
 import './index.css';
+
 // Using BrowserRouter since it provides clean URLs (no hashes in URLs)
 import { BrowserRouter as Router } from "react-router-dom";
 import ErrorBoundary from "./ErrorBoundary";
+
+//Google analytics
+ReactGA.initialize('G-2MS99Z9PLE');
 
 // Create the root
 const root = ReactDOM.createRoot(document.getElementById("root"));
